8b40b8c6f22a05c6db23c57f9e1781dc2d32877a
alanzzzzz <w604503352@163.com>
2018-05-31 17:33:17 +0800

    code done

---
+++ js/code.js
@@ -0,0 +1,19 @@
+$(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        effect : 'coverflow',
+        slidesPerView: 1.5,
+        centeredSlides: true,
+        initialSlide:2,
+
+            // 如果需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+
+        })
+
+
+
+
+});
